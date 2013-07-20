e8a06fbb66a856915716c1803c1abdc7340745ae
Jeremy Goh <jg485host@gmail.com>
2013-07-20 13:17:45 +0100

    added conditional for database setup

--- database.rb
+++ database.rb
@@ -3,7 +3,7 @@ require 'pg'
 require "dm-postgres-adapter"
 
 # DataMapper::setup(:default, 'postgres://localhost/tennis-rps-app')
-DataMapper::setup(:default,ENV["HEROKU_POSTGRESQL_PINK_URL"])
+DataMapper::setup(:default,ENV["HEROKU_POSTGRESQL_PINK_URL"] || 'postgres://localhost/tennisrps')
 
 class Shot
   include DataMapper::Resource
